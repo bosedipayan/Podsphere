1b91474a1b705e14ede30d603f3aeaf2506a4adf
Dipayan Bose <bosedipayan2000@gmail.com>
2025-02-03 22:33:58 +0530

    Setup and started modification of webapp

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const RightSidebar = () => {
-  return <div>Rightsidebar</div>;
+  return <section className="right_sidebar text-white-1">RightSidebar</section>;
 };
 
 export default RightSidebar;
